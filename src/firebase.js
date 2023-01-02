f6edabf431bb1c26de4587e53707208b87e3b1ea
Andrew Aiello <andrewaiello7@gmail.com>
2023-01-03 00:48:43 +1100

    Updates firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,25 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
-const {
-    VITE_API_KEY,
-    VITE_AUTH_DOMAIN,
-    VITE_DATABASE_URL,
-    VITE_PROJECT_ID,
-    VITE_STORAGE_BUCKET,
-    VITE_MESSAGING_SENDER_ID,
-    VITE_APP_ID,
-} = import.meta.env
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_AUTH_DOMAIN,
-  databaseURL: VITE_DATABASE_URL,
-  projectId: VITE_PROJECT_ID,
-  storageBucket: VITE_STORAGE_BUCKET,
-  messagingSenderId: VITE_MESSAGING_SENDER_ID,
-  appId: VITE_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
